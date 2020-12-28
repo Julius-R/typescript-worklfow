0e676b702b9ff8381fec1cb65b0001ad264b1913
Julius-R <julius.robinson95@yahoo.com>
2020-12-27 23:10:11 -0600

    made a typescript project to test

--- src/js/app.ts
+++ src/js/app.ts
@@ -1,15 +1,32 @@
-const log = value => console.log(value);
+// Basic types in Typescript
 
-log("This works")
+// Boolean
+let isDone: boolean = false;
 
+// Number
+let decimal: number = 3.50;
+let num : number = 5;
 
+// String
+let color: string = "blue";
 
+// Arrays 
+let list: number[] = [1,2,3];
+let listDow: Array<number> = [4,5,6];
 
+// Tuple 
+let x: [string, number]; //
+x = ["hello", 10,];
+// x = [10, "hey"]; won't work
 
-
-
-
-
+// Enum
+enum Color {
+    Red,
+    Green,
+    Blue,
+}
+let c: Color = Color.Green;
+console.log(c)
 
 
 
